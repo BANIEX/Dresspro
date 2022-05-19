9f6fb1c205503d335994ad2ff0a40501376bdb9b
BANIEX <banjibaniex1@gmail.com>
2022-05-19 14:58:33 +0100

    second update

---
+++ app.js
@@ -0,0 +1,7 @@
+let backgroundImage = document.getElementById("bg_image");
+
+backgroundImage.className = "crop_top"
+let crop_top = document.getElementById("crop_top")
+crop_top.style.display = "inline"
+
+// alert("How far")
